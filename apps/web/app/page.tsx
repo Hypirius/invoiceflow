2e7ce6686942b0652e511d7a7886d916a89a1a8e
Hypirius <zuhayerhossain25@gmail.com>
2026-05-23 18:07:32 +0600

    Initial next project setup and bloat clear

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Page() {
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24"></main>
-  )
+  return <main className=""></main>;
 }
