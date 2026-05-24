337c753395f94d28bab10d24b5db17e9c9da38b9
Hypirius <zuhayerhossain25@gmail.com>
2026-05-24 13:23:50 +0600

    feat(web): add reusable button with select presents for ui use

---
+++ apps/web/src/components/ui/Button.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, ComponentPropsWithoutRef } from "react";
+import { JetBrains_Mono } from "next/font/google";
+import { cn } from "../../utils/cn";
+
+const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
+
+interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
+  variant: "primary" | "inverted" | "secondary" | "outlined";
+  children: ReactNode;
+  height?: string;
+  width?: string;
+  className?: string;
+}
+
+export default function Button({
+  variant,
+  children,
+  height = "100",
+  width = "50",
+  className,
+}: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        `${jetBrainsMono.className} ${height} ${width}`,
+        {
+          "bg-[#4F46E5] text-[#F8FAFC]": variant === "primary",
+          "bg-[#0F172A] text-[#F8FAFC]": variant === "inverted",
+          "bg-[#64748B] text-[#0F172A]": variant === "secondary",
+          "bg-[#F8FAFC] text-[#0F172A] border-[#0F172A]":
+            variant === "outlined",
+        },
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
+}
