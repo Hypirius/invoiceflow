ddf6c0ed02ea614f564aae0ecd8e79f8ceb48101
Hypirius <zuhayerhossain25@gmail.com>
2026-05-27 23:30:53 +0600

    refactor(web): change menu/main component sizing to auth main for better reusability by children

--- apps/web/src/features/auth/components/AuthMain.tsx
+++ apps/web/src/features/auth/components/AuthMain.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export default function AuthMain({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-[#FFFFFF] h-full w-[55%]">
-      <section className="size-full flex items-center justify-center">
+    <div className="bg-[#FFFFFF] h-full w-[55%] flex items-center justify-center">
+      <section className="w-100 h-120 flex flex-col justify-center text-[#191C1E] gap-5">
         {children}
       </section>
     </div>
--- apps/web/src/features/auth/login/components/LoginMenu.tsx
+++ apps/web/src/features/auth/login/components/LoginMenu.tsx
@@ -6,12 +6,12 @@ import SignUpRedirect from "./SignUpRedirect";
 
 export default function LoginMenu() {
   return (
-    <div className=" w-100 h-120 flex flex-col justify-center text-[#191C1E] gap-5">
+    <>
       <LoginHeader />
       <GoogleLogin />
       <Divider text="or" />
       <LoginForm />
       <SignUpRedirect />
-    </div>
+    </>
   );
 }
