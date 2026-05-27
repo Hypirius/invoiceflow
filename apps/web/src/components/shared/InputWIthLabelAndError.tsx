53d18f9a09aa3a1a1f304ab24687b383afe735d4
Hypirius <zuhayerhossain25@gmail.com>
2026-05-27 23:17:09 +0600

    feat(web): add input with label that has error message support

---
+++ apps/web/src/components/shared/InputWIthLabelAndError.tsx
@@ -0,0 +1,32 @@
+import { ComponentPropsWithoutRef } from "react";
+import InputWithLabel from "../ui/InputWithLabel";
+
+type InputWithLabelAndErrorProps = ComponentPropsWithoutRef<"input"> & {
+  id: string;
+  labelText: string;
+  labelClassName?: string;
+  className?: string;
+  error?: string;
+};
+
+export default function InputWithLabelAndError({
+  id,
+  labelText,
+  labelClassName,
+  className,
+  error,
+  ...rest
+}: InputWithLabelAndErrorProps) {
+  return (
+    <div>
+      <InputWithLabel
+        id={id}
+        labelText={labelText}
+        labelClassName={labelClassName}
+        className={className}
+        {...rest}
+      />
+      {error && <p className="mt-2 text-[#F44336]">{error}</p>}
+    </div>
+  );
+}
