cfd2c19d43173c6e0fbc4d92c9bb82645a248b14
Hypirius <zuhayerhossain25@gmail.com>
2026-05-27 23:42:23 +0600

    refactor(web): move separate loginMenu file and components to within the page.tsx to avoid extra wrapper components

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,5 +1,17 @@
-import LoginMenu from "@/features/auth/login/components/LoginMenu";
+import Divider from "@/components/shared/Divider";
+import GoogleOAuthButton from "@/components/shared/GoogleOAuthButton";
+import LoginHeader from "@/features/auth/login/components/LoginHeader";
+import LoginForm from "@/features/auth/login/components/LoginForm";
+import SignUpRedirect from "@/features/auth/login/components/SignUpRedirect";
 
-export default function Page() {
-  return <LoginMenu />;
+export default function LoginMenu() {
+  return (
+    <>
+      <LoginHeader />
+      <GoogleOAuthButton />
+      <Divider text="or" />
+      <LoginForm />
+      <SignUpRedirect />
+    </>
+  );
 }
