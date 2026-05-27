a0b5ca5de515bb4478223bf2b31158ba52251053
Hypirius <zuhayerhossain25@gmail.com>
2026-05-27 23:14:57 +0600

    feat(web): add reusable section divider component

---
+++ apps/web/src/components/shared/Divider.tsx
@@ -0,0 +1,9 @@
+export default function Divider({ text }: { text: string }) {
+  return (
+    <div className="flex items-center justify-around text-center">
+      <div className="border-[#C7C4D8] w-[45%] h-0.5 border"></div>
+      <span className="mx-1.5 w-[10%] text-[#464555]">{text}</span>
+      <div className="border-[#C7C4D8] w-[45%] h-0.5 border"></div>
+    </div>
+  );
+}
