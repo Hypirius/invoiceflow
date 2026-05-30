09198e45a435a011c9023d40c28a706ec234f67b
Hypirius <zuhayerhossain25@gmail.com>
2026-05-30 13:51:42 +0600

    style(web): change default height value to a more appropiate value

--- apps/web/src/components/ui/Input.tsx
+++ apps/web/src/components/ui/Input.tsx
@@ -8,7 +8,7 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
 export default function Input({ className, ...rest }: InputProps) {
   return (
     <input
-      className={cn("w-80 h-40 border border-[#C7C4D8]", className)}
+      className={cn("w-80 h-10 border border-[#C7C4D8]", className)}
       {...rest}
     />
   );
