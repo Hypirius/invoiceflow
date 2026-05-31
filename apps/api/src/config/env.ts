c0d506da59d8bb896f14fc0387b474fd150e8813
Hypirius <zuhayerhossain25@gmail.com>
2026-05-31 20:22:56 +0600

    chore(api): add db url support and todo tasks

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -15,12 +15,16 @@ const configSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "tests"]),
   ROOT_FOLDER_PATH: z.string(),
   PORT: z.number().default(5000),
+  DATABASE_URL: z.string(),
 });
 
 const config = configSchema.parse({
   NODE_ENV,
   ROOT_FOLDER_PATH: pathToEnv,
   PORT: Number(process.env.PORT),
+  DATABASE_URL: process.env.DATABASE_URL,
 });
 
 export default config;
+
+// TODO: More refines can be used to ensure exact intended matches of properties, I.E: constrainting DB url from string to exact postresql link
