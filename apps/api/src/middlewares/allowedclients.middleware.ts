3c0d7008b3755074ed77a491a32d6944a7d5f2b8
Hypirius <zuhayerhossain25@gmail.com>
2026-06-03 21:49:50 +0600

    refactor(api): change to use ForbiddenError class which allows for better management of values

--- apps/api/src/middlewares/allowedclients.middleware.ts
+++ apps/api/src/middlewares/allowedclients.middleware.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import config from "../config/env";
+import { ForbiddenError } from "@/lib/errors/ErrorClasses";
 
 function allowedClients() {
   return (req: Request, res: Response, next: NextFunction) => {
     if (!config.WHITELIST_CLIENTS.includes(req.url)) {
-      res.send({ error: "Forbidden, client is not whitelisted" }).status(403);
+      return next(new ForbiddenError());
     }
 
     next();
