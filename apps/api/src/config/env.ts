c05d4358ba7f388285ac76251b17b4e0d05e112b
Hypirius <zuhayerhossain25@gmail.com>
2026-06-04 12:13:20 +0600

    feat(api): add support for jwt secrets and expire properties

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -19,6 +19,9 @@ const configSchema = z.object({
   DATABASE_URL: z.string(),
   WHITELIST_CLIENTS: z.array(z.url()),
   BCRYPT_SALT_ROUNDS: z.number(),
+  JWT_SECRET_KEY: z.string(),
+  JWT_ACCESS_EXPIRES_IN: z.string().or(z.number()),
+  JWT_REFRESH_EXPIRES_IN: z.string().or(z.number()),
 });
 
 const config = configSchema.parse({
@@ -28,8 +31,10 @@ const config = configSchema.parse({
   DATABASE_URL: process.env.DATABASE_URL,
   WHITELIST_CLIENTS: JSON.parse(process.env.WHITELIST_CLIENTS as string),
   BCRYPT_SALT_ROUNDS: Number(process.env.BCRYPT_SALT_ROUNDS),
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
+  JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN,
 });
-
 export default config;
 
 // TODO: More refines can be used to ensure exact intended matches of properties, I.E: constrainting DB url from string to exact postresql link
