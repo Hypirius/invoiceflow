c423eb7b29f1a75115e9619253a18a1d47871671
Hypirius <zuhayerhossain25@gmail.com>
2026-06-05 00:01:02 +0600

    feat(api); add cookie parser middleware and package to parse cookie strings into usable objects

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -8,6 +8,7 @@ import setRequestIdHeader from "./middlewares/setReqIdHeader.middleware";
 import { httpLogger } from "./middlewares/httpLogger.middleware";
 import globalErrorMiddleware from "./middlewares/globalError.middleware";
 import authRouter from "./features/auth/auth.route";
+import cookieParser from "cookie-parser";
 
 const app: Express = express();
 
@@ -17,6 +18,7 @@ app.use(httpLogger);
 app.use(allowedClients());
 app.use(cors({ origin: config.WHITELIST_CLIENTS }));
 app.use(express.json());
+app.use(cookieParser());
 app.use("/v1", authRouter);
 app.use(globalErrorMiddleware);
 
