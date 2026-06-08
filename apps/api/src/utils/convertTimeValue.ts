9b9dbdf4b1740c3bbca9945970fae892ee22c9eb
Hypirius <zuhayerhossain25@gmail.com>
2026-06-09 00:01:01 +0600

    fix(api): change type information to avoid constant ms type importing

--- apps/api/src/utils/convertTimeValue.ts
+++ apps/api/src/utils/convertTimeValue.ts
@@ -1,11 +1,11 @@
 import ms from "ms";
 
-function convertTimeValue(value: ms.StringValue | number) {
+function convertTimeValue(value: unknown) {
   if (typeof value === "number") {
     return value;
   }
 
-  return ms(value);
+  return ms(value as ms.StringValue);
 }
 
 export default convertTimeValue;
