384b3d7221cbd9b5e06f62de09783ff2e7387336
Hypirius <zuhayerhossain25@gmail.com>
2026-06-10 22:16:50 +0600

    feat(api): add oauth support and trimming of fields

--- packages/zod-schema/src/auth/signUp.schema.ts
+++ packages/zod-schema/src/auth/signUp.schema.ts
@@ -1,27 +1,45 @@
 import z from "zod";
 
-const userSignUpPrimaryDetailsSchema = z.object({
-  email: z.email(),
-  fullName: z.string(),
-  password: z
-    .string()
-    .regex(/^.{8,}$/, "Password must be greater than 8 characters")
-    .regex(/.*\d/, "Password must contain atleast one digit")
-    .regex(/.*[a-z]/, "Password must contain atleast one lowercase character")
-    .regex(/.*[A-Z]/, "Password must contain atleast one uppercase character")
-    .regex(
-      /.*[@$#!%*?&^]/,
-      "Password must contain atleast one special character",
-    )
-    .regex(
-      /^[A-Za-z\d@$#!%*?&^]+$/,
-      "Password can only use A-Z, a-z, digits and @$!%*?& characters",
-    ),
-});
+const userSignUpPrimaryDetailsSchema = z
+  .object({
+    email: z.email().trim(),
+    fullName: z.string().trim(),
+    password: z
+      .string()
+      .regex(/^.{8,}$/, "Password must be greater than 8 characters")
+      .regex(/.*\d/, "Password must contain atleast one digit")
+      .regex(/.*[a-z]/, "Password must contain atleast one lowercase character")
+      .regex(/.*[A-Z]/, "Password must contain atleast one uppercase character")
+      .regex(
+        /.*[@$#!%*?&^]/,
+        "Password must contain atleast one special character",
+      )
+      .regex(
+        /^[A-Za-z\d@$#!%*?&^]+$/,
+        "Password can only use A-Z, a-z, digits and @$!%*?& characters",
+      )
+      .trim()
+      .optional(),
+
+    oauthId: z.string().trim().optional(),
+  })
+  .refine(
+    (data) => {
+      if (!data.oauthId && !data.password) return false;
+    },
+    {
+      message: "Either password or oauth must be passed, fields missing",
+      path: ["password, oauthId"],
+    },
+  )
+  .refine((data) => !(data.password && data.oauthId), {
+    message: "Sign up details cannot have both oauthId and password.",
+    path: ["password", "oauthId"],
+  });
 
 const userSignUpSecondaryDetailsSchema = z.object({
-  displayName: z.string().optional(),
-  profileImage: z.url().optional(),
+  displayName: z.string().trim().optional(),
+  profileImage: z.url().trim().optional(),
 });
 
 const userSignUpDetailsSchema = z.object({
