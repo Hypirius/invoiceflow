de03d8fcf2ead2241abc688d141a6b4671fb7889
Hypirius <zuhayerhossain25@gmail.com>
2026-06-14 21:45:30 +0600

    refactor(web): change from set amount of height to full height

--- apps/web/src/features/auth/components/AuthMain.tsx
+++ apps/web/src/features/auth/components/AuthMain.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function AuthMain({ children }: { children: ReactNode }) {
   return (
     <div className="bg-[#FFFFFF] h-full w-[55%] flex items-center justify-center">
-      <section className="w-100 h-120 flex flex-col justify-center text-[#191C1E] gap-5">
+      <section className="w-100 h-full flex flex-col justify-center items-center text-[#191C1E] gap-5">
         {children}
       </section>
     </div>
