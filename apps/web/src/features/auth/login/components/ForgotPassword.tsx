4a871a35b4152bc498891e9c60246b490bd9c98f
Hypirius <zuhayerhossain25@gmail.com>
2026-06-14 21:46:45 +0600

    feat(web): add font size and margin bottom for separation

--- apps/web/src/features/auth/login/components/ForgotPassword.tsx
+++ apps/web/src/features/auth/login/components/ForgotPassword.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ForgotPassword() {
   return (
-    <Link href="/forgot-password" className="text-[#3525CD]">
+    <Link href="/forgot-password" className="text-[#3525CD] mb-3 text-[14px]">
       Forgot Password?
     </Link>
   );
