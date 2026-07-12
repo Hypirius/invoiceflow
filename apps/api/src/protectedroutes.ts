69cd9d6ee9aa65f685b42aa4a4daecf51ba5a7c2
Hypirius <zuhayerhossain25@gmail.com>
2026-07-12 22:56:59 +0600

    feat(api): update to use new routes

--- apps/api/src/protectedroutes.ts
+++ apps/api/src/protectedroutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import checkAuth from "./features/auth/checkAuth.middleware";
+import changeDetailsController from "./features/auth/change-details/changeDetails.controller";
+import organisationRouter from "./features/organisation/routes";
 
 const protectedRouter: Router = Router();
 
 protectedRouter.use(checkAuth());
+protectedRouter.post("/auth/change-details", changeDetailsController);
+protectedRouter.use("/organisation", organisationRouter);
 
 export default protectedRouter;
