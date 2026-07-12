0648bcc6b7897179c7bd4e11bccd3cc276f40f4d
Hypirius <zuhayerhossain25@gmail.com>
2026-07-12 21:54:31 +0600

    refactor(web): add centralized url store for easier control of url endpoints

---
+++ apps/web/src/config/fetchUrls.ts
@@ -0,0 +1,10 @@
+const fetchUrls = {
+  cloudinary: "https://api.cloudinary.com/v1_1/usercreationsystem/image/upload",
+  signature: "http://localhost:3000/v1/upload-image",
+  signUp: "http://localhost:3000/v1/auth/sign-up",
+  login: "http://localhost:3000/v1/auth/login",
+  organisation: "http://localhost:3000/v1/organisation",
+  changeDetails: "http://localhost:3000/v1/auth/change-details",
+};
+
+export default fetchUrls;
